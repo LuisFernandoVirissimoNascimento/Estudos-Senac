7853362cd59ebe4f22d30dfdc35403ab535aa2d4
Luis Fernando <luisvirissimo789@gmail.com>
2024-08-20 09:24:48 -0400

    Cadastro feito em tkinter
    Fiz um mini cadastro em tkinter

--- 150824/janela.py
+++ 150824/janela.py
@@ -8,13 +8,18 @@ def goofyaaah():
     global numbah
     numbah += 1
     print(numbah)
+    labelU.configure(text=f"{numbah}")
+    botaoU.configure(text=f"repete oxente{numbah}")
 
 root = Tk()
 frm = ttk.Frame(root, padding=100)
 frm.grid()
 
 # Button that calls the function when clicked
-ttk.Label(frm, text=f"{numbah}").grid(column=0, row=0)
-ttk.Button(frm, text=f"repete oxente{numbah}", command= goofyaaah).grid(column=1, row=0)
+labelU = ttk.Label(frm, text=f"{numbah}")
+labelU.grid(column=0, row=0)
+
+botaoU = ttk.Button(frm, text=f"repete oxente{numbah}", command= goofyaaah)
+botaoU.grid(column=1, row=0)
 
 root.mainloop()
---
+++ Tkinter Studies/Registration.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+def show_popup():
+    loginCheck = login.get()
+    senhaCheck = senha.get()
+    senhaConfirmCheck = senhaConfirm.get()
+    if loginCheck == "":
+        messagebox.showerror("Erro", "Login está vazio")
+        return
+        
+    if senhaCheck == "":
+        messagebox.showerror("Erro", "Senha está vazio")
+        return
+    
+    if senhaConfirmCheck != senhaCheck:
+        messagebox.showerror("Erro", "Senha não é igual a confirmação de senha")
+        return
+    
+    if senhaCheck == loginCheck:
+        messagebox.showerror("Erro", "Login e senha não podem ser os mesmos")
+        return
+    
+    
+    messagebox.showinfo("Parabens", "Cadastro feito com sucesso !")
+    
+
+
+
+# Create the main application window
+root = Tk()
+root.geometry("720x480")
+root.title("Main Window")
+changesToWindow = Frame(root, background= 'grey')
+changesToWindow.pack(fill="both",anchor='center',expand=True)
+
+loginText = Label(changesToWindow, text= "Login :",fg= 'white',font=('Comic Sans MS',20),justify= 'left',anchor= 'center',background= "gray")
+loginText.pack(pady=5)
+login = Entry(changesToWindow)
+login.pack(pady=20)
+
+senhaText = Label(changesToWindow, text= "Senha :",fg= 'white',font=('Comic Sans MS',20),justify= 'left',anchor= 'center',background= "gray")
+senhaText.pack(pady=5)
+senha = Entry(changesToWindow)
+senha.pack(pady=20)
+
+senhaConfirmText = Label(changesToWindow, text= "Confirmar Senha :",fg= 'white',font=('Comic Sans MS',20),justify= 'left',anchor= 'center',background= "gray")
+senhaConfirmText.pack(pady=5)
+senhaConfirm = Entry(changesToWindow)
+senhaConfirm.pack(pady=20)
+# Create a button that will trigger the popup
+button = Button(changesToWindow, text="Click Me!", command=show_popup)
+button.pack(pady=20)
+
+
+root.mainloop()
